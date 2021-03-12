f9e60826056835b6aa64e0074b1934de2217740c
tianza <tianza@mingyuanyun.com>
2021-03-12 20:50:26 +0800

    add certificate

--- client/client.go
+++ client/client.go
@@ -4,16 +4,49 @@ import (
 	"context"
 	"github.com/tianzuoan/grpc-go-demo/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 )
 
 const (
-	ADDRESS = ":10051"
+	ADDRESS = ":8080"
 )
 
 func main() {
+	grpcServerWithCredential()
+}
+
+func grpcServerInsecure() {
 	//通过grpc 库 建立一个连接
-	conn, err := grpc.Dial(":10051", grpc.WithInsecure())
+	conn, err := grpc.Dial(ADDRESS, grpc.WithInsecure())
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	//通过刚刚的连接 生成一个client对象。
+	c := service.NewTinaClient(conn)
+	//直接通过 client对象 调用 服务端的函数
+	r, err := c.SayHello(context.Background(), &service.HelloRequest{Name: "马德福"})
+	if err != nil {
+		log.Fatal("tina service could not say hello:", err)
+	}
+	log.Printf("tina say hello: %s", r.Message)
+	langReply, err := c.LearnForeignLanguage(context.Background(), &service.LanguageRequest{Kind: "chinese"})
+	if err != nil {
+		log.Fatal("could not learn language,reason is :%v", err)
+	}
+	log.Printf("learn language,the score is:%v", langReply.Score)
+}
+
+func grpcServerWithCredential() {
+	//创建凭证
+	transportCredentials, err := credentials.NewClientTLSFromFile("../keys/server.crt", "yncmall.cn")
+	if err != nil {
+		log.Fatal("certificated authorized failed!", err)
+	}
+	//通过grpc 库 建立一个连接（带凭证）
+	conn, err := grpc.Dial(ADDRESS, grpc.WithTransportCredentials(transportCredentials))
 	if err != nil {
 		return
 	}
--- server/server.go
+++ server/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tianzuoan/grpc-go-demo/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 )
 
-const PORT = ":10051"
+const PORT = ":8080"
 
 func main() {
+	grpcServerWithHttp2()
+}
+
+func grpcServerInsecure() {
 	//监听端口
 	lis, err := net.Listen("tcp", PORT)
 	if err != nil {
@@ -27,4 +34,60 @@ func main() {
 	log.Printf("grpc server start at: %v", PORT)
 	//处理链接
 	_ = grpcServer.Serve(lis)
+}
+
+func grpcServerWithCredential() {
+	//监听端口
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		return
+	}
+	//创建https通行证
+	transportCredentials, err := credentials.NewServerTLSFromFile("../keys/server.crt", "../keys/server_no_password.key")
+	if err != nil {
+		log.Fatal("certificate created failed!")
+	}
+	//创建一个grpc 服务器（带证书）
+	grpcServer := grpc.NewServer(grpc.Creds(transportCredentials))
+	//注册事件
+	service.RegisterTinaServer(grpcServer, service.NewTinaService())
+
+	//gRPC服务反射,方便grpcurl调试
+	reflection.Register(grpcServer)
+
+	log.Printf("grpc server start at: %v", PORT)
+	//处理链接
+	_ = grpcServer.Serve(lis)
+}
+
+func grpcServerWithHttp2() {
+	//创建https通行证
+	transportCredentials, err := credentials.NewServerTLSFromFile("../keys/server.crt", "../keys/server_no_password.key")
+	if err != nil {
+		log.Fatal("certificate created failed!")
+	}
+	//创建一个grpc 服务器（带证书）
+	grpcServer := grpc.NewServer(grpc.Creds(transportCredentials))
+	//注册事件
+	service.RegisterTinaServer(grpcServer, service.NewTinaService())
+
+	//gRPC服务反射,方便grpcurl调试
+	reflection.Register(grpcServer)
+
+	log.Printf("grpc server start at: %v", PORT)
+	//处理链接
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println(request.Proto) //HTTP/2.0
+		fmt.Println(request.RequestURI)
+		fmt.Println(request)
+		grpcServer.ServeHTTP(writer, request)
+	})
+	//创建http服务
+	httpServer := http.Server{
+		Addr:    PORT,
+		Handler: mux,
+	}
+	//http服务启动监听并带证书认证
+	_ = httpServer.ListenAndServeTLS("../keys/server.crt", "../keys/server_no_password.key")
 }
